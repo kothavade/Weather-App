f2bdfc081478410b3f7e31ea8a9642bc9cb797a8
Ved Kothavade <kothavade.ved@gmail.com>
2022-01-12 19:03:33 -0500

    functioning get + parse w/ coroutines and HttpURLConnection

--- app/src/main/java/com/example/weatherproject/MainActivity.kt
+++ app/src/main/java/com/example/weatherproject/MainActivity.kt
@@ -1,40 +1,97 @@
 package com.example.weatherproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.weatherproject.databinding.ActivityMainBinding
-import com.google.gson.JsonObject
-import org.json.JSONException
-import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Response
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.Json.Default.parseToJsonElement
+import kotlinx.serialization.json.jsonObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(R.layout.activity_main)
+        val zipCode = "08852"
+        val key = "a5cf2a811a281fccc912a1b0e55c03e2"
+        val country = "us"
+        val units = "imperial"
 
-        val service = ApiInterface.create()
 
-        val call: Call<JsonObject> = service.fetchFact()
-        call.enqueue(object : retrofit2.Callback<JsonObject> {
+        val urlString = "https://api.openweathermap.org/data/2.5/weather?zip=$zipCode,$country&appid=$key&units=$units"
 
-            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
-                val response = response.body()
-                // rest of the keys...
-            }
+        lifecycleScope.launch {
+            val result = httpGet(urlString)
+            val dataJSON = parseToJsonElement(result).jsonObject
+            //val main = dataJSON.jsonObject.get(:)
+            findViewById<TextView>(R.id.location).text= dataJSON.toString()
+//            val main = dataJSON["main"]
+//            val temp = main["temp"]
+//            findViewById<TextView>(R.id.temp).text=
 
-            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
-            }
-        })
 
-        temp.text(response.)
+        }
+//        val dataJSON = Json.decodeFromString<weatherData>(dataString)
+//        findViewById<TextView>(R.id.location).text = dataJSON.name
+//        findViewById<TextView>(R.id.temp) = dataJSON.main.temp.toString()
+
+
+
+
+
+
+    }
+
+    private suspend fun httpGet(myURL: String): String {
+
+        val result = withContext(Dispatchers.IO) {
+            val inputStream: InputStream
+
+
+            // create URL
+            val url: URL = URL(myURL)
+
+            // create HttpURLConnection
+            val conn: HttpURLConnection = url.openConnection() as HttpURLConnection
+
+            // make GET request to the given URL
+            conn.connect()
+
+            // receive response as inputStream
+            inputStream = conn.inputStream
+
+            // convert inputstream to string
+            convertInputStreamToString(inputStream)
+
+
+        }
+        return result
+
+    }
+
+    private fun convertInputStreamToString(inputStream: InputStream): String {
+        val bufferedReader: BufferedReader? = BufferedReader(InputStreamReader(inputStream))
+
+        var line:String? = bufferedReader?.readLine()
+        var result:String = ""
+
+        while (line != null) {
+            result += line
+            line = bufferedReader?.readLine()
+        }
+
+        inputStream.close()
+        return result
     }
 }
