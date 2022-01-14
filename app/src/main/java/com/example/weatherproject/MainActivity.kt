9a8fcc1be60a217c0f557cea8006aec4908a52e0
Ved Kothavade <kothavade.ved@gmail.com>
2022-01-14 00:56:30 -0500

    implement second api for more information from zip (state, county)

--- app/src/main/java/com/example/weatherproject/MainActivity.kt
+++ app/src/main/java/com/example/weatherproject/MainActivity.kt
@@ -1,48 +1,128 @@
 package com.example.weatherproject
 
 import android.os.Bundle
-import android.widget.TextView
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.CoroutineDispatcher
+import com.example.weatherproject.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json.Default.parseToJsonElement
+import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
 import java.io.BufferedReader
+import java.io.FileNotFoundException
 import java.io.InputStream
 import java.io.InputStreamReader
+import java.lang.IllegalArgumentException
 import java.net.HttpURLConnection
 import java.net.URL
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val zipCode = "08852"
-        val key = "a5cf2a811a281fccc912a1b0e55c03e2"
+        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        var stateInfo = ""
+        var zipCode = ""
+        val zipCodeKey = "9f25f690-74ea-11ec-b891-e59a9acda4e4"
+        var zipValid: Boolean = true
         val country = "us"
-        val units = "imperial"
+        binding.button.setOnClickListener{
+            binding.button.isEnabled=false
+            val zipAPI = lifecycleScope.launch{
+                zipCode = binding.input.text.toString()
+                Log.d("TAG","zipCode: $zipCode")
+                val zipCodeString = "https://app.zipcodebase.com/api/v1/search?apikey=$zipCodeKey&codes=$zipCode&country=${country.uppercase()}"
+                Log.d("TAG","zipCodeString: $zipCodeString")
+                val result = httpGet(zipCodeString)
+                Log.d("TAG","result: $result")
+                val dataJSON = parseToJsonElement(result)
+                Log.d("TAG","dataJson: $dataJSON")
+                try{
+                    if(dataJSON.jsonObject.get("results")?.jsonObject?.get(zipCode)!=null){
+                        zipValid=true
+                        stateInfo = dataJSON.jsonObject.get("results")?.jsonObject?.get(zipCode)?.jsonArray?.get(0)?.jsonObject?.get("state_code").toString().replace("\"", "") + ", ${country.uppercase()}"
+                        Log.d("TAG","country is true")
+                    }
+                    else {
+                        zipValid = false
+                    }
+                }catch (_:IllegalArgumentException){
+                    zipValid=false
+                }
 
-
-        val urlString = "https://api.openweathermap.org/data/2.5/weather?zip=$zipCode,$country&appid=$key&units=$units"
-
-        lifecycleScope.launch {
-            val result = httpGet(urlString)
-            val dataJSON = parseToJsonElement(result).jsonObject
-            //val main = dataJSON.jsonObject.get(:)
-            findViewById<TextView>(R.id.location).text= dataJSON["name"].toString().replace("\"", "")
-            val temperature = dataJSON.get("main")?.jsonObject?.get("temp")
-            if(temperature!=null) {
-                findViewById<TextView>(R.id.temp).text = "$temperature°F"
             }
 
+            // zipCheck = """^\d{5}(-\d{4})?$""".toRegex()
+            lifecycleScope.launch{
+                zipAPI.join()
+                Log.d("TAG","zipAPIJOINED")
+                if(zipValid){
+                    Log.d("TAG","ZIP IS VALID")
+
+                    zipCode = binding.input.text.toString()
+
+                    val key = "b78ef799a58e72c75824c2eeb7077223"//"a5cf2a811a281fccc912a1b0e55c03e2"
+                    val units = "imperial"
+                    val exclude = "minutely,hourly,daily"
+
+                    val weatherString = "https://api.openweathermap.org/data/2.5/weather?zip=$zipCode,$country&appid=$key&units=$units"
+
+
+                    try {
+                        val result = httpGet(weatherString)
+                        val dataJSON = parseToJsonElement(result)
+                        println(dataJSON.toString())
+                        Log.d("TAG","cod: ${dataJSON.jsonObject.get("cod").toString()}")
+
+                        binding.location.text =
+                            dataJSON.jsonObject.get("name").toString().replace("\"", "") + ", " + stateInfo
+
+                        val lat = dataJSON.jsonObject.get("coord")?.jsonObject?.get("lat").toString()
+                        val lon = dataJSON.jsonObject.get("coord")?.jsonObject?.get("lon").toString()
+                        val oneCallString =
+                            "https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&appid=$key&exclude=$exclude&units=$units"
+                        val result2 = httpGet(oneCallString)
+                        val dataJSON2 = parseToJsonElement(result2)
+                        //binding.temp.text =
+                        //    dataJSON2.jsonObject.get("current")?.jsonObject?.get("weather")?.jsonArray?.get(0)?.jsonObject?.get("description").toString().replace("\"", "").capitalizeWords()
+                        val temp = dataJSON2.jsonObject.get("current")?.jsonObject?.get("temp").toString().toDouble()
+                        val tempAsInt = temp.toInt()
+                        binding.temp.text =
+                            "${tempAsInt.toString()}°F"
+                    }
+                    catch(_: FileNotFoundException) {
+                        Toast.makeText(applicationContext,"Invalid Zip Code. Try Again.", Toast.LENGTH_LONG).show()
+                        stateInfo = ""
+                        binding.temp.text = ""
+                        binding.location.text = ""
+                    }
+                }
+                else {
+                    Toast.makeText(applicationContext,"Invalid Zip Code. Try Again.", Toast.LENGTH_LONG).show()
+                    stateInfo = ""
+                    binding.temp.text = ""
+                    binding.location.text = ""
+                }
+            }
+            binding.button.isEnabled=true
         }
+
+
     }
+
+    private fun String.capitalizeWords(): String = split(" ").joinToString(" ") { it.capitalize() }
 
     private suspend fun httpGet(myURL: String): String {
         val result = withContext(Dispatchers.IO) {
