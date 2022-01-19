aa8ac4ccaaa59b1ae0fd51d1db53d4283f92d468
Ved Kothavade <kothavade.ved@gmail.com>
2022-01-19 18:21:36 -0500

    bug fix and time fix

--- app/src/main/java/com/example/weatherproject/MainActivity.kt
+++ app/src/main/java/com/example/weatherproject/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.weatherproject
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
@@ -19,7 +18,6 @@ import java.io.BufferedReader
 import java.io.FileNotFoundException
 import java.io.InputStream
 import java.io.InputStreamReader
-import java.lang.Exception
 import java.net.HttpURLConnection
 import java.net.URL
 import java.text.SimpleDateFormat
@@ -41,24 +39,25 @@ class MainActivity : AppCompatActivity() {
         val country = "us"
 
         fun setLoading() {
-            binding.location.text="Loading..."
-            binding.coord.text="Loading..."
+            val loading = "Loading..."
+            binding.location.text=loading
+            binding.coord.text=loading
 
 
-            binding.temp1.text="Loading..."
-            binding.temp2.text="Loading..."
-            binding.temp3.text="Loading..."
-            binding.temp4.text="Loading..."
+            binding.temp1.text=loading
+            binding.temp2.text=loading
+            binding.temp3.text=loading
+            binding.temp4.text=loading
 
-            binding.desc1.text="Loading..."
-            binding.desc2.text="Loading..."
-            binding.desc3.text="Loading..."
-            binding.desc4.text="Loading..."
+            binding.desc1.text=loading
+            binding.desc2.text=loading
+            binding.desc3.text=loading
+            binding.desc4.text=loading
 
-            binding.time1.text="Loading..."
-            binding.time2.text="Loading..."
-            binding.time3.text="Loading..."
-            binding.time4.text="Loading..."
+            binding.time1.text=loading
+            binding.time2.text=loading
+            binding.time3.text=loading
+            binding.time4.text=loading
 
             Glide.with(this)
                 .load(R.drawable.loading)
@@ -73,6 +72,37 @@ class MainActivity : AppCompatActivity() {
                 .load(R.drawable.loading)
                 .into(binding.imageView4)
 
+            binding.imageView.setImageDrawable(null)
+
+        }
+        fun setError() {
+            binding.location.text="Waiting..."
+            binding.coord.text=""
+
+            binding.temp1.text=""
+            binding.temp2.text=""
+            binding.temp3.text=""
+            binding.temp4.text=""
+
+            binding.desc1.text=""
+            binding.desc2.text=""
+            binding.desc3.text=""
+            binding.desc4.text=""
+
+            binding.time1.text=""
+            binding.time2.text=""
+            binding.time3.text=""
+            binding.time4.text=""
+
+            binding.imageView1.setImageDrawable(null)
+            binding.imageView2.setImageDrawable(null)
+            binding.imageView3.setImageDrawable(null)
+            binding.imageView4.setImageDrawable(null)
+
+            Glide.with(this)
+                .load(R.drawable.loading)
+                .into(binding.imageView)
+
         }
 
         binding.button.setOnClickListener {
@@ -86,8 +116,6 @@ class MainActivity : AppCompatActivity() {
                 try {
                     val result = httpGet(weatherString)
                     val dataJSON = parseToJsonElement(result)
-                    println(dataJSON.toString())
-                    Log.d("TAG", "cod: ${dataJSON.jsonObject.get("cod").toString()}")
 
                     binding.location.text =
                         dataJSON.jsonObject.get("name").toString().replace("\"", "")
@@ -97,35 +125,13 @@ class MainActivity : AppCompatActivity() {
                     binding.coord.text = "Coordinates: $lat, $lon"
                     val oneCallString =
                         "https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&appid=$key&exclude=$exclude&units=$units"
-                    Log.d("Tag", oneCallString)
                     val result2 = httpGet(oneCallString)
                     val dataJSON2 = parseToJsonElement(result2)
-                    Log.d("Tag", dataJSON2.toString())
-
-                    val rightNow = Calendar.getInstance()
-                    var currHour = rightNow.get(Calendar.HOUR_OF_DAY)
-                    var hour1  = 0
-                    var hour2 = 0
-                    var hour3 = 0
-
-                    if(currHour+12 <=23)
-                        currHour+=12
-                    else currHour-=12
-
-                    if (currHour+1 <= 23)
-                        hour1 = currHour+1
-                    else hour1 = 23-currHour
-                    if (hour1+1 <= 23)
-                        hour2 = hour1+1
-                    else hour2 = 23-hour1
-                    if (hour2+1 <= 23)
-                        hour3 = hour2+1
-                    else hour3 = 23-hour2
 
                     for (i in 0..3) {
                         when (i) {
                             0 -> {
-                                val temp = dataJSON2.jsonObject.get("hourly")?.jsonArray?.get(currHour)?.jsonObject
+                                val temp = dataJSON2.jsonObject.get("hourly")?.jsonArray?.get(0)?.jsonObject
                                 val timeString = temp?.get("dt").toString().toLong()
                                 val time = SimpleDateFormat("hh:mm a").format((timeString * 1000))
                                 val weather = temp?.get("weather")?.jsonArray?.get(0)?.jsonObject
@@ -139,11 +145,11 @@ class MainActivity : AppCompatActivity() {
                                 Glide.with(this@MainActivity)
                                     .load(imgString)
                                     .into(binding.imageView1)
-                                //binding.imageView1.setImageBitmap(imgGet(imgString))
+                                //binding.imageView1.setImageBitmap(bitmapGet(imgString))
                                 binding.desc1.text = desc.capitalizeWords()
                             }
                             1 -> {
-                                val temp = dataJSON2.jsonObject.get("hourly")?.jsonArray?.get(hour1)?.jsonObject
+                                val temp = dataJSON2.jsonObject.get("hourly")?.jsonArray?.get(1)?.jsonObject
                                 val timeString = temp?.get("dt").toString().toLong()
                                 val time = SimpleDateFormat("hh:mm a").format((timeString * 1000))
                                 val weather = temp?.get("weather")?.jsonArray?.get(0)?.jsonObject
@@ -157,11 +163,11 @@ class MainActivity : AppCompatActivity() {
                                 Glide.with(this@MainActivity)
                                     .load(imgString)
                                     .into(binding.imageView2)
-                                //binding.imageView2.setImageBitmap(imgGet(imgString))
+                                //binding.imageView2.setImageBitmap(bitmapGet(imgString))
                                 binding.desc2.text = desc.capitalizeWords()
                             }
                             2 -> {
-                                val temp = dataJSON2.jsonObject.get("hourly")?.jsonArray?.get(hour2)?.jsonObject
+                                val temp = dataJSON2.jsonObject.get("hourly")?.jsonArray?.get(2)?.jsonObject
                                 val timeString = temp?.get("dt").toString().toLong()
                                 val time = SimpleDateFormat("hh:mm a").format((timeString * 1000))
                                 val weather = temp?.get("weather")?.jsonArray?.get(0)?.jsonObject
@@ -175,11 +181,11 @@ class MainActivity : AppCompatActivity() {
                                 Glide.with(this@MainActivity)
                                     .load(imgString)
                                     .into(binding.imageView3)
-                                //binding.imageView3.setImageBitmap(imgGet(imgString))
+                                //binding.imageView3.setImageBitmap(bitmapGet(imgString))
                                 binding.desc3.text = desc.capitalizeWords()
                             }
                             3 -> {
-                                val temp = dataJSON2.jsonObject.get("hourly")?.jsonArray?.get(hour3)?.jsonObject
+                                val temp = dataJSON2.jsonObject.get("hourly")?.jsonArray?.get(3)?.jsonObject
                                 val timeString = temp?.get("dt").toString().toLong()
                                 val time = SimpleDateFormat("hh:mm a").format((timeString * 1000))
                                 val weather = temp?.get("weather")?.jsonArray?.get(0)?.jsonObject
@@ -193,36 +199,37 @@ class MainActivity : AppCompatActivity() {
                                 Glide.with(this@MainActivity)
                                     .load(imgString)
                                     .into(binding.imageView4)
-                                //binding.imageView4.setImageBitmap(imgGet(imgString))
+                                //binding.imageView4.setImageBitmap(bitmapGet(imgString))
                                 binding.desc4.text = desc.capitalizeWords()
                             }
                         }
                     }
                 } catch (_: FileNotFoundException) {
                     Toast.makeText(applicationContext, "Invalid Zip Code. Try Again.", Toast.LENGTH_LONG).show()
-                    setLoading()
+                    setError()
                 }
             }
             binding.button.isEnabled = true
         }
     }
 
-    private suspend fun httpGet(myURL: String): String {
+    private suspend fun httpGet(inputURL: String): String {
         val result = withContext(Dispatchers.IO) {
             val inputStream: InputStream
-            val url = URL(myURL)
+            val url = URL(inputURL)
             val conn: HttpURLConnection = url.openConnection() as HttpURLConnection
             conn.connect()
             inputStream = conn.inputStream
-            convertInputStreamToString(inputStream)
+            inputStreamToString(inputStream)
         }
         return result
     }
 
-    private suspend fun imgGet(myURL: String): Bitmap {
+    //Using Glide for images instead due to animated Gif support, but this works just as well for static images
+    private suspend fun bitmapGet(inputURL: String): Bitmap {
         val inputStream: InputStream
         withContext(Dispatchers.IO) {
-            val url = URL(myURL)
+            val url = URL(inputURL)
             val conn: HttpURLConnection = url.openConnection() as HttpURLConnection
             conn.connect()
             inputStream = conn.inputStream
@@ -230,7 +237,7 @@ class MainActivity : AppCompatActivity() {
         return BitmapFactory.decodeStream(inputStream)
     }
 
-    private fun convertInputStreamToString(inputStream: InputStream): String {
+    private fun inputStreamToString(inputStream: InputStream): String {
         val bufferedReader: BufferedReader = BufferedReader(InputStreamReader(inputStream))
         var line: String? = bufferedReader.readLine()
         var result = ""
